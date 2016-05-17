a924e77dff462a0e08acc8589f7c38d2021bebbe
Nicolas-Montoro <nicolas.montoro@u-psud.fr>
2016-05-17 19:24:44 +0200

    Etape 4 - Gestion des redirections

--- src/commands.c
+++ src/commands.c
@@ -13,126 +13,160 @@ void addInput(char *commande,char *pathFileHistory){
 	}
 	//Sinon on ecrit la commande a l'interieur de celui-ci.
 	write(fileDestination,commande,strlen(commande));
+	write(fileDestination,"\n",strlen("\n"));
 	close(fileDestination);
 }
 
 
 int readCommand(char *pathFileHistory){	
-	char buffer[COMMAND_MAX_LENGTH];
+	char * buffer= NULL;
+    char *entryPosition = NULL;
+	int *positionRedirection=malloc(sizeof(int)*ARG_MAX_NUMBER-1);
 	
-	if (fgets(buffer, 200, stdin)) // Commande entrée
-	{	int taille=0;
-		char** command = parseString(buffer,&taille);
+	buffer=malloc(COMMAND_MAX_LENGTH*sizeof(char));
+	int nb_pipes=0;
+	int nb_redirections=0;
+
+    if(fgets(buffer, COMMAND_MAX_LENGTH, stdin)){
+        /*on cherche le \n ajoute automatiquement par fgets*/
+        entryPosition = strchr(buffer, '\n');
+        /*si on en trouve un, on le remplace par \0*/
+        if (entryPosition != NULL) *entryPosition = '\0';
+
+		char ** command=parseStringPipesAndRedirections(buffer,&nb_pipes,&nb_redirections,positionRedirection);
 		addInput(buffer,pathFileHistory);
-		
 		if(strcmp(command[0], "exit") == 0)
 		{
 			freeStringTable(command); // Libération de la liste de commandes
 			return 0;
 		}
-		executeCommand(command,taille,pathFileHistory);
-		freeStringTable(command); // Libération de la liste de commandes
 		
+		executeCommand(command,nb_pipes,nb_redirections,positionRedirection,pathFileHistory);
+		freeStringTable(command); // Libération de la liste de commandes
+		free(buffer);
+		free(positionRedirection);
 		return 1;
 	}
-	else // CTRL+D utilisé: fin du programme
+	else{// CTRL+D utilisé: fin du programme
 		return 0;
+	}
 }
 
-int executeCommand(char **command, int nb_pipes ,char * startingDirectory) {
-    int i = 0;
-    int j = 0;
-    int s = 1;
-    int pid;
-    int status;
-    char *path=NULL;	//Chemin contenant la commande a executer
-    int place;	//place dans la chaine de la commande a executer
-    int pipefds[2*nb_pipes];//Tableau pour pouvoir piper les commandes
-    const int commands = nb_pipes + 1;	//Nombre de commandes a executer 
-    int commandStarts[ARG_MAX_NUMBER];	//Table des positions des commandes a exectuer
+int executeCommand(char** command,int nb_pipes,int nb_redirections,int *positionRedirection ,char * pathFileHistory){
+	int i=0;
+	int j=0;
+	int pid; // Permet de réaliser le fork 
+ 	int status; // Permet d'attendre les différents processus
+	int pipes[nb_pipes*2]; // Permet de réaliser les pipes 
+	int out, in;//Correspondant au fichier pour les redirections 
+	int saved_stdin = dup(0), saved_stdout = dup(1); // Permet de sauvergarder les entrées et sorties standards
+	char **command2; // Permet de redecouper chaque commande avec ses arguments
+	char * path=malloc(sizeof(char)*COMMAND_MAX_LENGTH); // Permet de trouver le chemin de chaque commande
+
+	command2=parseString(command[i]);// On decoupe une premiere fois la 1ere commande pour voir s'il ne s'agit pas de cd 
 	
-	commandStarts[0] = 0;
-	
-	//S'il sagit de la commande cd on ne fait pas de fork et on l'execute directement
-	if(strcmp(command[0], "cd") == 0)
+	//S'il s'agit de cd 
+	if(strcmp(command2[0], "cd") == 0)
 	{
-		commandCd(command);
+		commandCd(command2);
 		return EXIT_SUCCESS;
 	}
-	//S'il n'est pas possible d'utiliser un pipe entre les expressions
-    for(i = 0; i < nb_pipes; i++){  
-        if(pipe(pipefds + i*2) < 0) {
-            perror("Couldn't Pipe");
+	
+	//On cree les pipes
+	for(i = 0; i < nb_pipes; i++){  
+		if(pipe(pipes + i*2) < 0) {
+			perror("Couldn't Pipe");
             exit(EXIT_FAILURE);
         }
-    }
-    
-	//On va chercher l'endroit ou commence chaque commande, en reperant les pipes
-	i=0;j=1;
-    while (command[i] != NULL){
-        if(!strcmp(command[i], "|")){
-            command[i] = NULL;
-            commandStarts[j] = i+1;
-            j++;
-        }
-        i++;
-    }
-    j=0;
-    //On va chercher le chemin des commandes si elles sont externes ,ensuite on va utiliser un fork,  puis on va les executer
-    for (i = 0; i < commands; ++i) {
-        place = commandStarts[i];
-        path=findPath(getenv("PATH"),command[place]);
-
-        pid = fork();
+	}
+	
+	//Gestion des redirections
+    for(i=0;i<nb_redirections;i++){
+	
+		//Case '<' : entrée standard
+		if(positionRedirection[i]<0){
+			in = open(command[-1*positionRedirection[i]], O_RDONLY);	// On ouvre le fichier en question 
+			dup2(in,0);	 // On lui attribue l'entrée standard 
+			close(in);	// On le ferme
+		}
+	
+		//Case '>' : sortie standard
+		else{
+			out = open(command[positionRedirection[i]], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);	// On ouvre le fichier en question et on le cree s'il n'existe pas
+			dup2(out,1);	//On lui attribue la sortie standard
+			close(out);	// On le ferme
+		}
+	}
+    //Gestion des pipes 
+    for (i = 0; i < nb_pipes+1; ++i) {	
+		if(i!=0){
+			path=NULL;
+			free(path);
+		}
+		for(j=0;j<ARG_MAX_NUMBER;j++){
+			command2[j]=NULL;
+			free(command2[j]);
+		}
+		free(command2);
+		
+		command2=parseString(command[i]); // On decoupe chaque commande avec ses arguments 
+		path=findPath(getenv("PATH"),command2[0]); // On cherche le chemin de chaque commande
         
+        pid = fork();
         if(pid == 0) {	// Processus fils: exécute la commande
+			//Ecriture
             if(i < nb_pipes){
-                if(dup2(pipefds[j + 1], 1) < 0){
-                    perror("dup2");
+                if(dup2(pipes[(i*2) + 1], 1) < 0){
                     exit(EXIT_FAILURE);
                 }
             }
-
-            if(j != 0 ){
-                if(dup2(pipefds[j-2], 0) < 0){
-                    perror("dup2");
+            //Lecture
+            if((i*2) != 0 ){
+                if(dup2(pipes[(i*2)-2], 0) < 0){
                     exit(EXIT_FAILURE);
                 }
             }
-
-            for(s = 0; s < 2*nb_pipes; s++){
-                    close(pipefds[s]);
+            //Fermeture
+            for(j= 0; j < 2*nb_pipes; j++){
+                    close(pipes[j]);
             }
-            if(strcmp(command[0], "touch") == 0)
-				commandTouch(command);
-			else if(strcmp(command[0], "cp") == 0)
-				commandCopy(command);
-			else if(strcmp(command[0], "cat") == 0)
-				commandCat(command);
-			else if(strcmp(command[0], "history") == 0){
-				commandHistory(command, startingDirectory);
-			}
-			else if(execv(path, command+place) < 0)
+            if(strcmp(command2[0], "touch") == 0) // S'il s'agit de la commande touch
+				commandTouch(command2);
+			else if(strcmp(command2[0], "cp") == 0) // S'il s'agit de la commande cp
+				commandCopy(command2);
+			else if(strcmp(command2[0], "cat") == 0) // S'il s'agit de la commande cat
+				commandCat(command2);
+			else if(strcmp(command2[0], "history") == 0) // S'il s'agit de la commande history
+				commandHistory(command2,pathFileHistory);
+			else if(execv(path, command2) < 0) // Sinon on execute la commande a l'aide de son chemin
 			{
 				printf("Erreur: Commande non existante...\n");
 				exit(0);
 			}
         }
-        else if(pid < 0){	// Processus inconnu
+        else if(pid < 0){	
+			// Process inconnu
             printf("Erreur: processus non reconnu...\n");
             exit(EXIT_FAILURE);
         }
-        j+=2;
     }
-
+	// Fermeture des pipes
     for(i = 0; i < 2 * nb_pipes; i++){
-        close(pipefds[i]);
+        close(pipes[i]);
     }
-
+ 
+	// Restauration de l'input/output de la console
+	dup2(saved_stdin, 0);
+	close(saved_stdin);
+	dup2(saved_stdout, 1);
+	close(saved_stdout);
+	
+	// Attente du processus 
     for(i = 0; i < nb_pipes + 1; i++){
         wait(&status);
     }
-    return 1;
+ 
+	return EXIT_SUCCESS;
 }
 
 
@@ -151,7 +185,8 @@ void freeStringTable(char** table){
 }
 
 
-char** parseString(char* string,int *taille){
+char** parseString(char* string){
+	
 	int i = 0; // Increment du numéro de caractère dans string
 	int j = 0; // Increment du numéro de chaîne de caractère
 	int k = 0; // Increment du numéro de caractère dans une chaîne
@@ -167,29 +202,80 @@ char** parseString(char* string,int *taille){
 	
 	while(string[i] == ' ') // On ignore les premiers espaces
 		i++;
-		
 	while(1) // Stoppé avec un return
 	{
-		if(string[i] == ' ' || string[i] == '\n') // Nouveau mot ou fin
+		if(string[i] == ' ' || string[i] == '\n' || string[i] == '\0') // Nouveau mot ou fin
 		{
 			buffer[j][k] = '\0'; // On termine la chaîne de caractères en cours
 			while(string[i] == ' ') // On ignore les espaces suivants
 				i++;
 			k = 0;
-			if(!strcmp(buffer[j],"|")){
-				*taille=*taille+1;
-			}
 			j++; // On passe à la chaîne de caractères suivante
-			if(string[i] == '\n'){ // Plus aucun mot: fin
+			if(string[i] == '\n' || string[i] == '\0') // Plus aucun mot: fin
 				return buffer;
-			}
 			// Allocation de la chaîne de caractère suivante
 			buffer[j] = malloc(ARG_MAX_LENGTH*sizeof(char));
 		}
 		else
-		{	
+		{
 			buffer[j][k] = string[i];
 			k++;
+			i++;
+		}
+	}
+}
+
+char ** parseStringPipesAndRedirections(char* string, int * nb_pipes,int *nb_redirections,int *positionRedirection){
+	
+	char ** buffer=calloc(ARG_MAX_LENGTH,sizeof(char*));
+	
+	int i = 0; // Increment du numéro de caractère dans string
+	int j = 0; // Increment du numéro de chaîne de caractère
+	int k = 0; // Increment du numéro de caractère dans une chaîne
+	
+	while(string[i] == ' ') // On ignore les premiers espaces
+		i++;
+	// Allocation du tableau de pointeur vers les chaînes de caactères	
+	buffer[0]=calloc(COMMAND_MAX_LENGTH,sizeof(char));
+	while(1) // Stoppé avec un return
+	{	
+		if(string[i]>=0){ // Si les caracteres sont bien representés
+			if(string[i] == '|' ||string[i] == '\n' || string[i] == '\0' || string[i] == '>' || string[i] == '<' ) // Nouveau mot ou fin
+			{			
+				buffer[j][k] = '\0'; // On termine la chaîne de caractères en cours
+				k = 0;
+				j++; // On passe à la chaîne de caractères suivante
+				if(string[i] == '\n'|| string[i] == '\0' ) // Plus aucun mot: fin
+				{
+					return buffer;
+				}
+				else if(string[i] == '>'){
+					positionRedirection[*nb_redirections]=j;
+					*nb_redirections=*nb_redirections+1;
+				}
+				else if(string[i] == '<'){
+					positionRedirection[*nb_redirections]=-1*j;
+					*nb_redirections=*nb_redirections+1;
+				}
+				else if(string[i] == '|'){
+					*nb_pipes=*nb_pipes+1;
+				}
+				i++;
+				// Allocation de la chaîne de caractère suivante
+				buffer[j]=calloc(COMMAND_MAX_LENGTH,sizeof(char));
+			}
+			else {	
+				if((k==0&&string[i]==' ')){ // Si des espaces se trouvent avant un nouveau mot 
+					i++;
+				}
+				else{
+					buffer[j][k] = string[i];
+					k++;
+					i++;
+				}					
+			}
+		}
+		else{
 			i++;
 		}
 	}
